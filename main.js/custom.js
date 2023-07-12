5cd2a2fc728021aa4f3e61b43ad8339bb87e7620
jankisojitra <jankisojitra0@gmail.com>
2023-07-12 17:16:49 +0530

    first commit

---
+++ main.js/custom.js
@@ -0,0 +1,9 @@
+$(".slick_slider").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 800,
+  prevArrow: '<i class="fa-solid fa-angle-left left_arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right right_arrow"></i>',
+});
